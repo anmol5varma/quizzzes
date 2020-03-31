6d7f2ae8613eaa27524243a43da3030dee69be80
Anmol Varma <anmol_varma@mckinsey.com>
2020-03-31 18:29:45 +0530

    add data in player list

--- getTeamLogoMapping.js
+++ getTeamLogoMapping.js
@@ -1,34 +1,32 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// const leagueId = [
-//     4328,   // English Premier League
-//     4331,   // German Bundesliga
-//     4332,   // Italian Serie A
-//     4334,   // French Ligue 1
-//     4335,   // Spanish La Liga
-//     4344,   // Portuguese Primeira Liga
-//     4346,   // American Major League Soccer
-//     4351,   // Brazilian Brasileirao
-//     4359    // Chinese Super League
-// ]
+const leagueId = [
+    4328,   // English Premier League
+    4331,   // German Bundesliga
+    4332,   // Italian Serie A
+    4334,   // French Ligue 1
+    4335,   // Spanish La Liga
+    4337,   // Dutch Eredivisie
+    4344,   // Portuguese Primeira Liga
+    4346,   // American Major League Soccer
+    4351,   // Brazilian Brasileirao
+    4359    // Chinese Super League
+]
 
-// const API_KEY = 1
-
-// Promise.all(
-//     leagueId.map(async lid => {
-//         const { data: { teams: teamList } } = await axios.get(`https://www.thesportsdb.com/api/v1/json/${API_KEY}/lookup_all_teams.php?id=${lid}`);
-//         return teamList.map(({ strTeam, strTeamBadge }) => ({
-//             name: strTeam.toLowerCase(),
-//             logo: strTeamBadge
-//         }))
-//     })
-// ).then(parsedTeamList => console.log(JSON.stringify(Array.prototype.concat.apply([], parsedTeamList))))
-
-const abc = require("./src/data/team-logo.json")
-
-const ans = {};
-
-abc.map(({ name, logo }) => ans[name] = logo);
-
-console.log(JSON.stringify(ans));
+const API_KEY = 1
+let abc;
+Promise.all(
+    leagueId.map(async lid => {
+        const { data: { teams: teamList } } = await axios.get(`https://www.thesportsdb.com/api/v1/json/${API_KEY}/lookup_all_teams.php?id=${lid}`);
+        return teamList.map(({ strTeam, strTeamBadge }) => ({
+            name: strTeam.toLowerCase(),
+            logo: strTeamBadge
+        }))
+    })
+).then(parsedTeamList => {
+    abc = Array.prototype.concat.apply([], parsedTeamList)
+    const ans = {};
+    abc.map(({ name, logo }) => ans[name] = logo);
+    console.log(JSON.stringify(ans));
+})
 
