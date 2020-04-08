d99c24fec3a6d1d67eae0e8f41ed14755bac80a1
Anmol Varma <anmol_varma@mckinsey.com>
2020-04-08 21:44:48 +0530

    add gatsby offline plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,11 +25,16 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/favicon.png`, // This path is relative to the root of the site.
+        icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: `gatsby-plugin-offline`,
+      options: {
+        precachePages: [`/`],
+      },
+    },
   ],
 }
