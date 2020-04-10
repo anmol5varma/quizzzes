b38bb382d3c249b93b65403fe187a1ab86d46c00
Anmol Varma <anmol_varma@mckinsey.com>
2020-04-11 03:25:57 +0530

    remove pre caching of images

--- gatsby-config.js
+++ gatsby-config.js
@@ -30,11 +30,6 @@ module.exports = {
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
-    {
-      resolve: `gatsby-plugin-offline`,
-      options: {
-        precachePages: [`/`],
-      },
-    },
+    `gatsby-plugin-offline`
   ],
 }
